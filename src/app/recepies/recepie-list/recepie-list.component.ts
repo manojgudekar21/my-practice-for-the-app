6dbafbf9bc708db299083e86e5e20d8db46c15ec
manoj gudekar <manojgudekar21@gmail.com>
2024-11-30 16:51:18 +0530

    added all the basic functionality and about to tadd the recepie details

---
+++ src/app/recepies/recepie-list/recepie-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recepie } from 'src/app/shared/recepie.model';
+
+@Component({
+  selector: 'app-recepie-list',
+  templateUrl: './recepie-list.component.html',
+  styleUrls: ['./recepie-list.component.css']
+})
+export class RecepieListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  recepies: Recepie[] = [
+    new Recepie("chicken curry", "its so delecious", "https://www.cubesnjuliennes.com/wp-content/uploads/2020/07/Instant-Pot-Chicken-Curry-Recipe.jpg"),
+    new Recepie("mutton curry", "its so awesome", "https://i0.wp.com/www.bharatzkitchen.com/wp-content/uploads/2018/11/mutton-curry-thumb.jpg?fit=800%2C450&ssl=1")
+  ]
+
+
+}
---
+++ src/app/shared/recepie.model.ts
@@ -0,0 +1,3 @@
+export class Recepie {
+    constructor(public recepie: string, public discription: string, public imagePath: string) { }
+}
